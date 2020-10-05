6f219758375a6b572a65bff287f85a351eaa089e
taewook kim <x_o_d_n@naver.com>
2020-10-05 22:20:38 +0900

    Add files via upload

--- assets/js/fixed-nav.js
+++ assets/js/fixed-nav.js
@@ -1,1 +1,34 @@
 /*! 구현중 */
+
+
+// var headerFix= function() {
+//     $(window).scroll(function() {
+//         var scroll=$(window).scrollTop();
+//         console.log(scroll);
+
+//         if(scroll > 0) {
+//             $("header").css({position:'fixed' });
+//         }else {
+//             $("header").css({position:'static'});
+//         }
+//     });
+// }
+
+
+
+
+
+   var headerFix= function() {
+        $(window).scroll(function() {
+            var scroll=$(window).scrollTop();
+            console.log(scroll);
+
+            if(scroll > 0) {
+                // $("header").css({position:'fixed',height:'100px' });
+                $("header").addClass('on');
+            }else {
+                // $("header").css({position:'static'});
+                $("header").removeClass('on');
+            }
+        });
+   }
